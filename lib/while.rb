46cceda8fd543c67a70ede8e9afdb1d79a010ba4
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 18:47:58 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,13 @@
 def using_while
   levitation_force = 6
 	while levitation_force < 10
-
+	  puts "Wingardium Leviosa"
+    counter += 1
 end
 
 counter = 0
 while counter < 20
-  puts "The current number is less than 20."
+
   counter += 1
 end
 
